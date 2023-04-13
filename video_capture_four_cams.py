f1686a82ad9a360ae42c2078837b17fc1fdb9f98
sino7 <louis.annabi@gmail.com>
2023-04-13 16:35:54 +0200

    fixed capture with four cameras

--- video_capture_four_cams.py
+++ video_capture_four_cams.py
@@ -1,45 +1,57 @@
 import cv2
 import time
+import argparse
+
+# Create the parser
+my_parser = argparse.ArgumentParser()
+
+# Add the arguments
+my_parser.add_argument('--cameras', type=int, default=4,
+                       help='le nombre de cameras', required=False)
+
+my_parser.add_argument('--resolution', type=int, default=2,
+                       help='par combien on divise la hauteur et largeur de la resolution des cameras')
+
+# Execute the parse_args() method
+args = my_parser.parse_args()
+n = args.cameras
 
 # Open a connection to the cameras
-cap0 = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap1 = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-cap2 = cv2.VideoCapture(2, cv2.CAP_DSHOW)
-cap3 = cv2.VideoCapture(3, cv2.CAP_DSHOW)
-caps = [cap0, cap1, cap2, cap3]
-
+caps = []
 sizes = []
 
-for cap in caps:
-    width = int(cap.get(3))//2
-    height = int(cap.get(4))//2
+for i in range(n):
+    cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
+    width = int(cap.get(3)//args.resolution)
+    height = int(cap.get(4)//args.resolution)
     size = (width, height)
     cap.set(3, width)
     cap.set(4, height)
+    caps.append(cap)
     sizes.append(size)
     
 # Compression
-lossless = cv2.VideoWriter_fourcc(* 'MJPG')
+lossless = cv2.VideoWriter_fourcc(*'MJPG')
 
 recording = False
 
 while True:
 
     # Capture a frame from the cameras
-    ret0, frame0 = cap0.read()
-    ret1, frame1 = cap1.read()
-    ret2, frame2 = cap2.read()
-    ret3, frame3 = cap3.read()
-
-    if ret0 and ret1 and ret2 and ret3:
-
-        frames = [frame0, frame1, frame2, frame3]
+    rets = []
+    frames = []
+    for i in range(n):
+        ret, frame = caps[i].read()
+        rets.append(ret)
+        frames.append(frame)
+        
+    # Check if the user pressed the enter key
+    pressedKey = cv2.waitKey(100) & 0xFF
+        
+    if all(rets):
         
         for i, frame in enumerate(frames):
             cv2.imshow("Webcam " + str(i), frame)
-            
-        # Check if the user pressed the enter key
-        pressedKey = cv2.waitKey(1) & 0xFF
         
         if pressedKey == ord('s') :
 
@@ -48,7 +60,7 @@ while True:
             # Save the frames as video files
             base_name = str(int(time.time()))
             file_names = [base_name + '_cam'+str(i) +'.avi' for i in range(len(caps))]
-            videos = [cv2.VideoWriter(file_name, lossless, 30, size) for (file_name, size) in zip(file_names, sizes)]
+            videos = [cv2.VideoWriter(file_name, lossless, 10, size) for (file_name, size) in zip(file_names, sizes)]
             recording = True
 
         if recording:
